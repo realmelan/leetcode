051ca3d354ec714a2d5ae186dee1a51ac613f9cb
realmelan <dingsong.net@gmail.com>
2020-02-10 21:37:49 -0800

    Create ___FILENAME___.cpp

---
+++ templates/___FILENAME___.cpp
@@ -0,0 +1,47 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+#include "common.h"
+using namespace std;
+
+namespace ___FILEBASENAME___ {
+/*
+// TODO: copy problem statement here
+
+*/
+class Solution {
+public:
+    // TODO: copy function signature here
+
+private:
+};
+}
+
+//*
+int main() {
+    // TODO: define parameter type here
+    struct param {
+    };
+    // TODO: prepare parameters here
+    vector<struct param> params {
+    };
+    // TODO: provide expected results here
+    vector<> answers {
+    };
+
+    for (auto& dp : params) {
+        cout << endl;
+        clock_t tstart = clock();
+        auto res = ___FILEBASENAME___::Solution().___FILEBASENAME___();
+        cout << res << endl;
+        cout << clock() - tstart << endl;
+    }
+
+    return 0;
+}
+//*/
